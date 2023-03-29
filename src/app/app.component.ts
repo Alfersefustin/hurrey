b11d55e4b2ffb1d27f77508af1b8b6a9d6d89d8a
Sebastin <alfersefustin97@gmail.com>
2023-03-29 15:02:17 +0530

    cord-11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Observable } from 'rxjs';
+import { debounceTime, scan, startWith, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'hurrey';
+  loader = true;
+  Show = true;
+  Show2 = false;
+  Show3 = false;
+  Show4 = false;
+  hide = true;
+  textControl = new FormControl();
+  reactiveForm: FormGroup;
+  statusClass = 'not-active';
+
+  setActiveClass() {
+    (this.statusClass = 'active'), '!active';
+  }
+  toggle1() {
+    this.Show2 = !this.Show2;
+    this.hide = false;
+    this.Show = false;
+  }
+  toggle2() {
+    this.Show3 = !this.Show3;
+    this.Show2 = false;
+    this.hide = false;
+    this.Show = false;
+  }
+  toggle3() {
+    this.Show4 = !this.Show4;
+    this.Show3 = false;
+    this.Show2 = false;
+    this.hide = false;
+    this.Show = false;
+  }
+
+  current = '';
+  history$: Observable<string[]>;
+  constructor(private builder: FormBuilder) {
+    this.history$ = this.textControl.valueChanges.pipe(
+      debounceTime(500),
+      startWith(this.current),
+      scan((acc, t) => (t ? acc.concat(t) : []), [])
+    );
+  }
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.loader = false;
+    }, 1000);
+    this.reactiveForm = this.builder.group({
+      username1: ['', Validators.required],
+    });
+  }
 }
